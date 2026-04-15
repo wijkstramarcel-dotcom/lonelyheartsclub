5d2351288151b96f4cd2b6c01ac9172cbb454afe
wijkstramarcel-dotcom <wijkstramarcel@hotmail.com>
2026-04-15 14:57:08 +0200

    Create supabase.js

---
+++ src/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient(
+  "https://kdkccffbvdrgqnvfkcqd.supabase.co",
+  "sb_publishable_vJkfyqOYDdAx7d0ggyERHA_nL6c8K0U"
+);
